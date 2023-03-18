d2220c495bbbdb2e5a68b3e339b1fc0337c9d6bf
juliand71 <julian7dixon1@gmail.com>
2023-03-18 17:14:48 -0400

    Updates

---
+++ precache-manifest.eb5b4b72637e10b62b58d2a70f4022f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d264bc77150512096ef543244ccb49",
+    "url": "/nutrition-calc/index.html"
+  },
+  {
+    "revision": "ffd4c9f8f924ae23a959",
+    "url": "/nutrition-calc/static/js/2.a318df18.chunk.js"
+  },
+  {
+    "revision": "d525fc20ced237340f5798bf946ff3a4",
+    "url": "/nutrition-calc/static/js/2.a318df18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb00315a4aacb690802a",
+    "url": "/nutrition-calc/static/js/main.138fff9b.chunk.js"
+  },
+  {
+    "revision": "b5d10c3538e76c945839",
+    "url": "/nutrition-calc/static/js/runtime-main.172f3921.js"
+  }
+]);
